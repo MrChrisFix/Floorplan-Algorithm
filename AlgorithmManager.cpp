2bf0bfe2d1020908d2569a39b3af32c3f0337f07
Christian <christian.fikus@wp.pl>
2022-11-21 21:54:40 +0100

    Remove unessesary commands

--- AlgorithmManager.cpp
+++ AlgorithmManager.cpp
@@ -79,7 +79,7 @@ void AlgorithmManager::Populate_H_Graph(GraphNode* currentNode)
 
 	for (auto left : currentType->left)
 	{
-		auto foundLeft = this->Graph_H->FindNodeByType(left); //is the type upwards already in the graph?
+		auto foundLeft = this->Graph_H->FindNodeByType(left); //is the type on the left already in the graph?
 		if (foundLeft != nullptr)
 			foundLeft->AddNodeToGraph(currentNode);
 		else
@@ -109,8 +109,6 @@ void AlgorithmManager::PopulateGraphs()
 		//H Graph
 		newNode = new GraphNode(type, false);
 		this->Populate_H_Graph(newNode);
-
-		//GraphNode* endHnode = new GraphNode(false, true);
 	}
 }
 
@@ -119,30 +117,6 @@ void AlgorithmManager::CreateTree()
 	std::vector<Variant*> VariantStack;
 
 	AddTreeBranch(0, VariantStack, this->treeRoot);
-
-	/* Old stuff
-	std::vector<TreeNode*> tempRoots;
-	for (int i = 0; i < this->types.size(); i++)
-	{
-		TreeNode* node = new TreeNode(nullptr, 0);
-
-		for (int j = 0; j < this->types[i]->GetVariants().size(); j++)
-		{
-			node->AddNewBranch(0);	//TODO: cost
-			//Create bush of options and then combine them together in a chain
-
-			//Note: ale jak i gdzie ja tu niby sprawdzam, czy te elementy pasuj¹ obok siebie?
-			//Chyba bêdê musia³ jeszcze przerobiæ to jakoœ ten Type, 
-			//¿eby wiedzieæ, co jest na prawo, lewo, w dó³, górê, ¿eby wykorzystaæ algorytm z zeszytu.
-
-			//Musia³bym chyba wtedy dla ka¿dego wêz³a osobno rozparzaæ sytauacjê(patrz zeszyt).
-			//Ale wci¹¿ muszê znaleŸæ algortym na ieracjê to tych wêz³ach aby dodawaæ nowe.
-			//Obawiam siê, ¿e bêdzie jednak potrzebna funkcja rekurencyjna
-		}
-		tempRoots.push_back(node);
-	}
-	//Next: combine together
-	*/
 }
 
 std::pair<unsigned, std::vector<Variant*>> AlgorithmManager::FindOptimal()
@@ -196,30 +170,6 @@ void AlgorithmManager::AddTreeBranch(unsigned int depth, std::vector<Variant*> &
 		}
 
 		variantStack.pop_back();
-
-		/* Old cost calculatons
-		unsigned int cost;
-		if (ptr->cost == UINT32_MAX)
-			cost = UINT32_MAX;
-		else
-		{
-			Check the value and if the elements are good size ergo use my notebook alg
-			if (ptr == nullptr)
-				cost = variant->GetCost();
-			else
-			{
-				bool doesFit = false;
-				doesFit = variant->Height() <= this->types[depth]->GetRequirements(); //TODO
-
-
-				if (doesFit)
-					cost = variant->GetCost();
-				else
-					cost = UINT32_MAX;
-			}
-		}
-		*/
 	}
-
 }
 
