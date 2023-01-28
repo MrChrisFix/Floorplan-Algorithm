bc8b43a5c90c9f308451ec49760858bf560ce862
Christian <christian.fikus@wp.pl>
2023-01-28 01:00:59 +0100

    Correction to rectangle placing order

--- Algorithm/RectanglePlacer.h
+++ Algorithm/RectanglePlacer.h
@@ -14,17 +14,7 @@ private:
 	std::map<Type*, Variant*> configuration;
 	Variant* blankVariant; //TODO: somehow manage memory of this object
 
-	void AddNextRect(int tlX, int tlY, GraphNode* node);
 	void AddRectangles();
-
-	enum class Intersection{
-		None,		// No intersecton
-		Fully,		// Fully inside of one => technicly shouldn't be possible, becouse neighbouring elemment shuld be at least Side intersecting
-		Side,		// Only one border is crossed
-		Corner,		// Corner crossing each other
-		Bullet,		// Two parrarel sides
-		Half		// 3 sides are in each other; like Side but the opposite rectangle involved
-	};
 
 public:
 	RectanglePlacer();
